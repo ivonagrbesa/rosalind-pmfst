67b840ba2da14ac13923cf086d896d6ac9a24236
ivonagrbesa <101057376+ivonagrbesa@users.noreply.github.com>
2022-08-28 20:27:33 +0200

    Create BA4A.py

---
+++ BA4A.py
@@ -0,0 +1,85 @@
+# fja vraca dictionary gdje je
+# (key, value)=(3-mer, aminokiselina)
+def get_genetic_code_codons():
+    genetic_code = {
+        "AAA": "K",
+        "AAC": "N",
+        "AAG": "K",
+        "AAU": "N",
+        "ACA": "T",
+        "ACC": "T",
+        "ACG": "T",
+        "ACU": "T",
+        "AGA": "R",
+        "AGC": "S",
+        "AGG": "R",
+        "AGU": "S",
+        "AUA": "I",
+        "AUC": "I",
+        "AUG": "M",
+        "AUU": "I",
+        "CAA": "Q",
+        "CAC": "H",
+        "CAG": "Q",
+        "CAU": "H",
+        "CCA": "P",
+        "CCC": "P",
+        "CCG": "P",
+        "CCU": "P",
+        "CGA": "R",
+        "CGC": "R",
+        "CGG": "R",
+        "CGU": "R",
+        "CUA": "L",
+        "CUC": "L",
+        "CUG": "L",
+        "CUU": "L",
+        "GAA": "E",
+        "GAC": "D",
+        "GAG": "E",
+        "GAU": "D",
+        "GCA": "A",
+        "GCC": "A",
+        "GCG": "A",
+        "GCU": "A",
+        "GGA": "G",
+        "GGC": "G",
+        "GGG": "G",
+        "GGU": "G",
+        "GUA": "V",
+        "GUC": "V",
+        "GUG": "V",
+        "GUU": "V",
+        "UAA": "*",
+        "UAC": "Y",
+        "UAG": "*",
+        "UAU": "Y",
+        "UCA": "S",
+        "UCC": "S",
+        "UCG": "S",
+        "UCU": "S",
+        "UGA": "*",
+        "UGC": "C",
+        "UGG": "W",
+        "UGU": "C",
+        "UUA": "L",
+        "UUC": "F",
+        "UUG": "L",
+        "UUU": "F",
+    }
+
+    return genetic_code
+  
+  
+  D=get_genetic_code_codons()
+  
+  def Translation(rna):
+  rj=""
+  for i in range(0, len(rna)-3+1, 3):
+    trimer=rna[i:i+3]
+    aminokis=D[trimer]
+    if aminokis=="*": #provjerimo je li stop kodon
+      break
+    else:
+      rj+=aminokis
+  return rj
